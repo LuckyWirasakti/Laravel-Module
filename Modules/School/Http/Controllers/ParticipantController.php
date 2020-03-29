628f302c8ae87dff50d7dbbdac5d824b82d481b8
Khoirur Rozaq <rozaqarmedian@gmail.com>
2020-03-29 16:38:09 +0700

    change response participant

--- Modules/School/Http/Controllers/ParticipantController.php
+++ Modules/School/Http/Controllers/ParticipantController.php
@@ -13,7 +13,7 @@ class ParticipantController extends Controller
 {
     public function index()
     {
-        return ParticipantResource::collection(Participant::with(['group','major','room'])->where('school_id', auth('school')->id())->get());
+        return ParticipantResource::collection(Participant::with(['group','major','room', 'school'])->where('school_id', auth('school')->id())->get());
     }
 
     public function Login(ParticipantRequest $request)
