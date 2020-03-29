858905bd686d5ed7a93c7601b6005556516cbc06
Khoirur Rozaq <rozaqarmedian@gmail.com>
2020-03-29 20:20:46 +0700

    change respone participant

--- Modules/Smartedu/Transformers/ParticipantResource.php
+++ Modules/Smartedu/Transformers/ParticipantResource.php
@@ -15,34 +15,27 @@ class ParticipantResource extends Resource
     public function toArray($request)
     {
 
-        $data = [];
-        $datanya = [
-                'group' => [
-                    'id' => $this->group->id,
-                    'name' => $this->group->name
-                ],
-                'major' => [
-                    'id' => $this->major->id,
-                    'name' => $this->major->name
-                ],
-                'room' => [
-                    'id' => $this->room->id,
-                    'name' => $this->room->name
-                ],
-                'school' => [
-                    'id' => $this->school->id,
-                    'name' => $this->school->name
-                ],
-            ];
-
-            array_push($data, $datanya);
-
         return [
             'id' => $this->id,
             'name' => $this->name,
             'nisn' => $this->nisn,
             'password' => $this->visible,
-            'data' => $data,
+            'group' => [
+                'id' => $this->group->id,
+                'name' => $this->group->name
+            ],
+            'major' => [
+                'id' => $this->major->id,
+                'name' => $this->major->name
+            ],
+            'room' => [
+                'id' => $this->room->id,
+                'name' => $this->room->name
+            ],
+            'school' => [
+                'id' => $this->school->id,
+                'name' => $this->school->name
+            ],
             'created_at' => $this->created_at->diffForHumans(),
             'updated_at' => $this->updated_at->diffForHumans()
         ];
