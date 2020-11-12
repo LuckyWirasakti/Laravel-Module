c0f55108721a9b402173851116851ac63ec6c62c
dvrg <15957000+dvrg@users.noreply.github.com>
2020-11-12 18:24:18 +0000

    add database seeder for school

--- Modules/School/Database/Seeders/SchoolDatabaseSeeder.php
+++ Modules/School/Database/Seeders/SchoolDatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Modules\School\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Support\Int;
+
 
 class SchoolDatabaseSeeder extends Seeder
 {
@@ -14,8 +19,21 @@ class SchoolDatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Model::unguard();
+        //Model::unguard();
 
         // $this->call("OthersTableSeeder");
+
+        DB::table('schools')->insert([
+            'id' => Int::random(1),
+            'name' => Str::random(10),
+            'province_id' => Int::random(1),
+            'regency_id' => Int::random(1),
+            'level_id' => Int::random(1),
+            'province_id' => Int::random(1),
+            'username' => Str::random(6),
+            'visible' => Str::random(6),
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+        ]);
     }
 }
